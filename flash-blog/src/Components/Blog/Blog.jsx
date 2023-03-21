5ac2f767d1126900d98046d7f983d7301f9bdf22
Paulcode2 <paullevites84@gmail.com>
2023-03-21 04:49:24 +0100

    set the components

---
+++ flash-blog/src/Components/Blog/Blog.jsx
@@ -0,0 +1,13 @@
+import Head from './Head/Headblog'
+import Body from './Body/Blogbody'
+import Footer from './Footer/Blogfoot' 
+
+function Blog (){
+    <>
+    <Head />
+    <Body />
+    <Footer />
+    </>
+}
+
+export default Blog
---
+++ flash-blog/src/Components/Blog/Body/Blogbody.jsx
@@ -0,0 +1,1 @@
+import './blogbosy.css'
